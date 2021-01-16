684796551a5b92fe7d3534ea607ebc5c2133381f
lampa-lana <lampa1979@mail.ru>
2021-01-16 11:20:06 +0300

    fix:mobile window

--- js/main.js
+++ js/main.js
@@ -5,5 +5,7 @@ menuButton.on('click', function () {
   console.log(' Клик по кнопке меню');
   $(".navbar-menu").toggleClass("navbar-menu--visible");
   $("body").toggleClass("no-scroll");
+  $(".menu-btn").toggleClass("menu-btn--close");
+    $(".navbar-top__menu-line").toggleClass("navbar-top__menu-line-invisible"); 
 });
   
