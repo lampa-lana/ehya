c6cf3c365948f8a839a2595f9f60aaf5bfd30c1e
lampa-lana <lampa1979@mail.ru>
2021-01-16 15:24:57 +0300

    create:adaptiv for mobile windows

--- js/main.js
+++ js/main.js
@@ -1,11 +1,73 @@
 $(document).ready(function () { });
- 
+ // мобильное меню
 var menuButton = $(".menu-btn");
 menuButton.on('click', function () {
   console.log(' Клик по кнопке меню');
   $(".navbar-menu").toggleClass("navbar-menu--visible");
   $("body").toggleClass("no-scroll");
   $(".menu-btn").toggleClass("menu-btn--close");
-    $(".navbar-top__menu-line").toggleClass("navbar-top__menu-line-invisible"); 
+    $(".navbar-top__menu-line").toggleClass("navbar-top__menu-line--invisible"); 
 });
   
+
+//модальное окно
+var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+   
+  }
+
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+  $(document).on('keydown', function (e) {
+    if (e.keyCode === 27) {
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+      document.getElementsByTagName("body")[0].style.overflow = "auto";
+     
+    }
+  });
+
+   $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Пожалуйста укажите Ваше имя",
+          minlength: "Ваше имя должно быть не менее двух букв",
+        },
+        email: {
+          required: "Пожалуйста укажите адрес Вашей почты",
+          email: "Ваша почта должна быть а формате name@domain.com",
+        },        
+        phone: {
+          required: "Пожалуйста укажите свой телефон",
+          phone: "Ваш телефон должен быть в формате of +7(999) 999-99-99",
+          minlength: "Ваш телефон должен состоять из 10 цифр",
+        },
+      },
+    });
+    
+  });
+  $(function(){  
+    $(".input__phone").mask("+7(999) 999-99-99", {
+      } );
+  }); 
